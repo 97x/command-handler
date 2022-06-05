61a8f2dbfdfec716b15c4ce3a2242b3ef8ac736c
Brandon Elliott <elliott.brandon1234@gmail.com>
2022-06-05 18:42:07 -0300

    added mongo support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
+const mongoose = require('mongoose')
+
 const CommandHandler = require('./CommandHandler')
 
 class Main {
-  constructor({ client, commandsDir }) {
+  constructor({ client, mongoUri, commandsDir }) {
     if (!client) {
       throw new Error('A client is required.')
+    }
+
+    if (mongoUri) {
+      this.connectToMongo(mongoUri)
     }
 
     if (commandsDir) {
       new CommandHandler(commandsDir, client)
     }
+  }
+
+  connectToMongo(mongoUri) {
+    mongoose.connect(mongoUri,  {
+      keepAlive: true,
+    })
   }
 }
 
